aff3c20cab03f95c69624925350d9c80f11f0af2
Alan Rehfeldt <alanrehfeldt@gmail.com>
2023-05-30 18:53:17 -0300

    sidebar finished

--- src/componentes/Sidebar.jsx
+++ src/componentes/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react'
+
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -6,14 +8,12 @@ export function Sidebar() {
             <img
                 className={styles.cover} 
                 src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZGV2ZWxvcGVyfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=50" 
-                alt="" 
             />
 
             <div className={styles.profile}>
                 <img
                     className={styles.avatar} 
-                    src="https://github.com/AlanRehfeldt.png" 
-                    alt="" />
+                    src="https://github.com/AlanRehfeldt.png" />
 
                 <strong>Alan Rehfeldt</strong>
                 <span>Web Developer</span>
@@ -21,6 +21,7 @@ export function Sidebar() {
 
             <footer>
                 <a href="#">
+                    <PencilLine size={20} />
                     Editar seu perfil
                 </a>
             </footer>
